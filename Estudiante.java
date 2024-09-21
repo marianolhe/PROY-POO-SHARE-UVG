3be607b88bef06887582e420eac3438181563d0e
liafdeb <biancka30ar@gmail.com>
2024-09-20 20:33:47 -0600

    crear clase subirPDF

---
+++ Estudiante.java
@@ -0,0 +1,15 @@
+public class Estudiante {
+    private String usuario;
+    private  String contrasena;
+    public String rol;
+    public  String carrera;
+
+    public Usuario(String usuario, String  contrasena, String rol, String carrera) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.rol = rol;
+        this.carrera = carrera;
+    }
+    
+    
+}
