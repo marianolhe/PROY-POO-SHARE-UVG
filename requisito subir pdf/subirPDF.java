b281992c4e557804d8236debfa82d5124a12343e
liafdeb <biancka30ar@gmail.com>
2024-09-28 19:55:48 -0600

    restauracion original

--- requisito subir pdf/subirPDF.java
+++ requisito subir pdf/subirPDF.java
@@ -13,6 +13,27 @@ public class subirPDF {
         this.carpetaDestino = carpetaDestino;
     }
 
+        // Método para subir y guardar el archivo a la carpeta predestinada
+        public void subirArchivo(String rutaArchivo) {
+            File archivo = new File(rutaArchivo);
+    
+            // Verificar que el archivo exista y sea un archivo PDF
+            if (archivo.exists() && archivo.isFile() && rutaArchivo.endsWith(".pdf")) {
+                try {
+                    // Definir la carpeta de destino para el PDF
+                    Path rutaDestino = Paths.get(carpetaDestino, archivo.getName());
+                    
+                    // Guardar el PDF en la carpeta de destino
+                    Files.copy(archivo.toPath(), rutaDestino);
+    
+                    System.out.println("Archivo subido correctamente a la carpeta :D");
+                } catch (IOException e) {
+                    System.out.println("Error al copiar el archivo: " + e.getMessage());
+                }
+            } else {
+                System.out.println("ERROR: asegurate que el archivo exista y sea un archivo PDF.");
+            }
+        }
 
 }
 
